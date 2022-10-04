23ebe782fae54a4ded3046d9be332b931a357c1e
IsabellaOHara <isabellaohara@btinternet.com>
2022-10-04 11:35:55 +0100

    added movie schema to screenings schema

--- src/schema/screenings.js
+++ src/schema/screenings.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 
+const MovieSchema = mongoose.Schema({
+
+    Name: String,
+    Actors: [String],
+    Director: String,
+    Genre: String,
+    "Release Date": String,
+    Duration: Number,
+    Classification: String
+})
+
 const ScreeningSchema = mongoose.Schema({
     CinemaName: {type:String, required:true},
     ScreenNum: {type:Number, required:true},
     ScreenType: {type:String, required:true},
-    Movie: {type:{Movie}, required:true},
+    Movie: {type:[MovieSchema], required:true},
     Date: {type:String, required:true},
     Time: {type:String, required:true},
     SeatsLeft: {type:Number, required:true}
