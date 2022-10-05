1e32282e70b32a55d4152a138742ddb6fa94c057
IsabellaOHara <isabellaohara@btinternet.com>
2022-10-05 11:23:10 +0100

    ScreeningsAPI work + json

--- backend/routes/screeningsAPI.js
+++ backend/routes/screeningsAPI.js
@@ -9,23 +9,25 @@ router.get("/getAll", (req, res) => {
     })
 })
 
-//filter by cinema name and movie name
-router.get("/getAllScreenings", (req, res) => {
-    ScreeningsModel.find({CinemaName:req.body.CinemaName}).then(c => {
+//filter by cinema name and movie name => only gives back dates and times
+router.get("/getDateAndTime", (req, res) => {
+    const byCinemaName = ScreeningsModel.find({$and: [ {CinemaName:req.body.CinemaName}, {MovieName:req.body.MovieName}]}, {_id:0, Date:1, Time:1}).then(c => {
         res.status(200).json(c)
     }).catch((err)=> {
         res.status(500).json(err)
     })
 })
 
-//filter by cinema name, movie name and date
+
+//filter by cinema name, movie name and date => only gives back time
 router.get("/getScreeningTimes", (req,res) => {
-    ScreeningsModel.find({CinemaName:req.body.CinemaName, Movie:req.body.Movie.Name, Date:req.body.Date}).then(d => {
-        res.status(200).json(c)
+    ScreeningsModel.find({$and: [ {CinemaName:req.body.CinemaName}, {MovieName:req.body.MovieName}, {Date:req.body.Date}]}, {_id:0, Time:1}).then(d => {
+        res.status(200).json(d)
     }).catch((err)=> {
         res.status(500).json(err)
     })
 })
+
 
 module.exports = router;
 
