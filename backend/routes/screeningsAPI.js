d4fa34eac8f7cbeb4e8879d1af0a1dd1211cfe96
IsabellaOHara <isabellaohara@btinternet.com>
2022-10-05 09:31:44 +0100

    screeningsAPI

--- backend/routes/screeningsAPI.js
+++ backend/routes/screeningsAPI.js
@@ -1,12 +1,31 @@
 const router = require("express").Router();
-const {ScreeningsModel} = require("../schema/screenings");
+const {ScreeningsModel} = require("../schema/screenings.js");
 
-router.get("/getAll", (req,res) => {
-    ScreeningsModel.find({CinemaName:req.body.CinemaName, Movie}).then(b => {
-        res.status(200).json(b)
+router.get("/getAll", (req, res) => {
+    ScreeningsModel.find().then(c => {
+        res.status(200).json(c)
+    }).catch((err)=> {
+        res.status(500).json(err)
+    })
+})
+
+//filter by cinema name and movie name
+router.get("/getAllScreenings", (req, res) => {
+    ScreeningsModel.find({CinemaName:req.body.CinemaName}).then(c => {
+        res.status(200).json(c)
+    }).catch((err)=> {
+        res.status(500).json(err)
+    })
+})
+
+//filter by cinema name, movie name and date
+router.get("/getScreeningTimes", (req,res) => {
+    ScreeningsModel.find({CinemaName:req.body.CinemaName, Movie:req.body.Movie.Name, Date:req.body.Date}).then(d => {
+        res.status(200).json(c)
     }).catch((err)=> {
         res.status(500).json(err)
     })
 })
 
 module.exports = router;
+
