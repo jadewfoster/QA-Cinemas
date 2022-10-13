b4f9d9bb11823cfb6dea9a9dd6550d160919c7e9
IsabellaOHara <isabellaohara@btinternet.com>
2022-10-13 16:12:11 +0100

    screens page

---
+++ src/components/Screens/ScreensCarouselStd/screensImagesStd.js
@@ -0,0 +1,18 @@
+const screensImagesStd = [
+{
+    id: 1,
+    src: "https://homemcr.org/app/uploads/2015/04/Cinema-2.jpg",
+    alt: "Standard screen seating plan"
+},
+{
+    id: 2,
+    src: 'https://britishcinematographer.co.uk/wp-content/uploads/2017/05/twitter-and-firstlook-onscreen.jpg',
+    alt: "Standard screen image 1"
+},
+{
+    id: 3,
+    src: "https://img.tagvenue.com/upload/60/eb/21071-screen-6-room.jpg",
+    alt: "Standard screen image 2"
+}
+];
+export default screensImagesStd;
