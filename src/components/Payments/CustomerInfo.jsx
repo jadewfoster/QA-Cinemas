c548009b7a1604061c8eb4c83b1c79776c32fb61
IsabellaOHara <isabellaohara@btinternet.com>
2022-10-13 14:39:35 +0100

    payments pages added react

--- src/components/Payments/CustomerInfo.jsx
+++ src/components/Payments/CustomerInfo.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
