6a975234ff394ce361a28d824f3ea18837c7fb75
jadewfoster <jadewfoster@gmail.com>
2022-10-14 11:21:04 +0100

    Styling Getting There pages

--- src/components/Getting There/LondonNorth.jsx
+++ src/components/Getting There/LondonNorth.jsx
@@ -4,34 +4,35 @@ export default function LondonNorth() {
 
     return(
         <div>
-        <h3>Directions to our London North cinema:</h3>
-
+        <h3 className="title">Directions to our London North cinema:</h3>
+        <div align="center">
         <iframe width="600" height="500" id="gmap_canvas"
         src="https://maps.google.com/maps?q=1%20Wakley%20St,%20London%20EC1V%207LT&t=&z=13&ie=UTF8&iwloc=&output=embed"
         frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-
-        <h4>Address:</h4>
+        </div>
+        <h4 className="subtitle">Address:</h4>
         <p>1 Wakley St, London EC1V 7LT</p>
-        <h4>Public transport:</h4>
-        <h5>By Tube</h5>
+        <h4 className="subtitle">Public transport:</h4>
+        <h5 className="smallsubtitle">By Tube</h5>
         <p>Our part of London is well-served by the London Underground.
             You can use the Northern Line (Bank branch), as well as the
             combination of the Circle/District/Metropolitan/Hammersmith 
             & City lines, and the Central Line.
             Our nearest tube stations are Angel, Farringdon and Moorgate.</p>
-        <h5>By Train</h5>
+        <h5 className="smallsubtitle">By Train</h5>
         <p>Our nearest train stations are Farringdon, Moorgate and
             King's Cross St Pancras.</p>
-        <h5>By Bus</h5>
+        <h5 className="smallsubtitle">By Bus</h5>
         <p>There are numerous bus routes which stop in City Road,
             Islington High Street and Pentonville Road, all a few minutes walk from 
             London North Cinema.
             Some of the nearest bus routes include 4, 56, 153, 43, 205, 214,
             394, 19, 38, 341.</p>
-        <h4>Parking information:</h4>
-        <p>he nearest National Car Parks are at Finsbury Square, Tabernacle Street, 
+        <h4 className="subtitle">Parking information:</h4>
+        <p>The nearest National Car Parks are at Finsbury Square, Tabernacle Street, 
             Clere Street and Paul Street. you can also book a
             parking space <a href="https://www.yourparkingspace.co.uk/">here</a>.</p>
+            <br/>
         </div>
     )
 }
--- src/components/Getting There/LondonSouth.jsx
+++ src/components/Getting There/LondonSouth.jsx
@@ -4,36 +4,37 @@ export default function LondonSouth() {
 
     return(
         <div>
-            <h3>Directions to our London South cinema:</h3>
-
+            <h3 className="title">Directions to our London South cinema:</h3>
+            <div align="center">
             <iframe width="600" height="500" id="gmap_canvas"
             src="https://maps.google.com/maps?q=20%20Peckham%20High%20St,%20London%20SE15%205DT&t=&z=13&ie=UTF8&iwloc=&output=embed"
             frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-
-        <h4>Address:</h4>
+</div>
+        <h4 className="subtitle">Address:</h4>
         <p>20 Peckham High St, London SE15 5DT</p>
-        <h4>Public transport:</h4>
-        <h5>By Overground</h5>
+        <h4 className="subtitle">Public transport:</h4>
+        <h5 className="smallsubtitle">By Overground</h5>
         <p>Our London South cinema is located within just 5 minutes walk from 
             Queens Road Peckham and Peckham Rye overground stations, which are 
             located on the Highbury&Islington - Clapham Junction line.
         </p>
-        <h5>By Train</h5>
+        <h5 className="smallsubtitle">By Train</h5>
         <p>Queens Road Peckham and Peckham Rye stations are both served by regular
             trains to Central London - London Bridge, Blackfriars and Victoria stations; as well as 
             Thameslink and Southeastern services across South London.
         </p>
-        <h5>By Bus</h5>
+        <h5 className="smallsubtitle">By Bus</h5>
         <p>There are many bus routes serving our London South location:
             12 Oxford St - Dulwich via Elephant & Castle, 37 Putney - Peckham via Clapham,
             36 Harrow - Peckham via Victoria/Camberwell/Oval,
             345 South Kensington - Peckham via Battersea/Brixton/Camberwell,
             63 Kings Cross - Honor Oak via Old Kent Road,
             78 Shorditch - Nunhead via Tower Bridge.</p>
-        <h4>Parking information:</h4>
+        <h4 className="subtitle">Parking Information:</h4>
         <p>The car park at rear of the cinema is FREE after 6:30pm,
             ALL day Sat/Sun and Bank holidays.
             80p per hour at all other times.</p>
+            <br></br>
     </div>
     )
 }
